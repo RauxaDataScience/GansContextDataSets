a0c5ef72bae624be98a7b65cc914c0446f8112c6
askorostelev <49989314+askorostelev@users.noreply.github.com>
2019-09-05 11:43:52 -0400

    adding python code and 3D model of chair

---
+++ 3DModel-to-2DImage-Generator.py
@@ -0,0 +1,29 @@
+#pip install numpy-stl
+
+from stl import mesh
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
+
+stl_mesh = mesh.Mesh.from_file('./chair.stl')
+
+def generate_save_figure(elev,azim,dist):         
+    figure = pyplot.figure(figsize=(1,1))
+    axes = mplot3d.Axes3D(figure)
+    axes.grid(False)
+    axes._axis3don=False    
+    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(stl_mesh.vectors))
+    scale = stl_mesh.points.flatten(-1)
+    axes.auto_scale_xyz(scale, scale, scale)
+    axes.view_init(elev=elev,azim=azim)
+    axes.dist = dist
+    axes.autoscale(True)
+    figure.savefig('./numpy-stl-images/elev({})-azim({})-dist({}).png'.format(elev,azim,dist))   
+    print('saved elev {}, azim {}, dist {}'.format(elev,azim,dist))
+    del figure,axes,scale
+    pyplot.close('all')
+
+for elev in range(0,180,1):
+    for azim in range(-360,360,1):
+        for dist in range(5,25,1):
+            generate_save_figure(elev,azim,dist)
+                
